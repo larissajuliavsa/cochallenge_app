2c6805218050b721ffc576b8b8950dcbe15693eb
larissajuliavsa <larissa.jvsa@gmail.com>
2023-05-19 23:41:00 -0300

    feat: :memo: error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { Error } from './pages/Error';
 import Home from './pages/Home';
 import { SignIn } from './pages/Signin';
 import { SignUp } from './pages/Signup';
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
-        {/* <Route path="*" element={<Error />} /> */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material';
+
+import error from '../../assets/img/errorImg.svg';
+
+export const Error = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        // flexDirection: '',
+        gap: '32px',
+        backgroundColor: '#E8E2E2',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography variant="h4" display="block" gutterBottom>
+        Acho que você <br /> se perdeu pelo caminho...
+      </Typography>
+      <Box>
+        <img style={{ width: '224px' }} src={error} alt="logo" />
+      </Box>
+    </Box>
+  );
+};
